33095fe603ca5161e283c6316197aeda9203764a
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-01-14 20:01:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const elem = document.getElementById
-const elems = document.querySelectorAll
+const elem = document.getElementById;
+const elems = document.querySelectorAll;
+var parsed;
+const soundList = new Array()
+const imageList = new Array()
+
+function goPressed() {
+  parsed = JSON.parse(elem("inp").value) || undefined
+  if (parsed !=== undefined) {
+    let sprites = parsed.targets
+    for (sprite of sprites) {
+      for (asset of sprites.costumes) {
+        imageList.push(JSON.parse(`["${asset.name}", "${asset.md5ext}"]`))
+      }
+      for (asset of sprites.sounds) {
+        soundList.push(JSON.parse(`{"${asset.name}": "${asset.md5ext}`))
+      }
+    }
+  for (asset of soundList) {
+    elems("body").innerHTML = elems("body").innerHTML + `<p class="output">AUDIO &quot;${asset[0]}&quot;: <a href="https://assets.scratch.mit.edu/${asset[1]}">view</a>`
+  }
+    
